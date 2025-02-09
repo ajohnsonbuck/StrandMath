5845705c03bbf74e327e22361d26988d265bafdb
Alex Johnson-Buck <alebuck@umich.edu>
2025-02-09 09:49:17 -0500

    Add function generate_reverse_motif.m for quickly generating reverse of NN motifs

---
+++ generate_reverse_motif.m
@@ -0,0 +1,15 @@
+function seqs_out = generate_reverse_motif(seqs)
+    seqs2 = cell(size(seqs,1),2);
+    seqs3 = cell(size(seqs,1),2);
+    seqs_out = cell(size(seqs));
+    for n = 1:length(seqs)
+        seqs2(n,:) = strsplit(seqs{n,1},'/');
+    end
+    for n = 1:length(seqs)
+        seqs3{n,1} = reverse(seqs2{n,2});
+        seqs3{n,2} = reverse(seqs2{n,1});
+    end
+    for n = 1:length(seqs)
+        seqs_out{n,1} = [seqs3{n,1},'/',seqs3{n,2}];
+    end
+end
